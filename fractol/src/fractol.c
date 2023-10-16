200f32475f9c3cc28eb94c87831f48730f52f99e
Alejandro Cárdenas Parejo <alejandrocp_952@hotmail.com>
2023-10-16 14:51:21 +0200

    día 1
    Investigación de mandelbrot y Julia

--- fractol/src/fractol.c
+++ fractol/src/fractol.c
@@ -6,7 +6,7 @@
 /*   By: alcarden <alcarden@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 13:30:46 by alcarden          #+#    #+#             */
-/*   Updated: 2023/10/16 13:41:13 by alcarden         ###   ########.fr       */
+/*   Updated: 2023/10/16 14:45:18 by alcarden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,29 @@
 #include <stdio.h>
 #include <memory.h>
 
+
+//  Z(n+1) = [Z(n)]^2 + C
+
+void mandelbrot(double a, double b, t_data*s)
+{
+	double x;
+	double y;
+	double temp;
+	int i;
+
+	x = 0;
+	y = 0;
+	i = 0;
+
+	while (x*x + y*y < 4 && i < s->max_iter)
+	{
+		temp = x*x - y*y + a;
+		y = 2*x*y + b;
+		x = temp;
+		i++;
+	}
+	if (i == s->max_iter)
+		s->img->pixels[s->img->width * s->y + s->x] = 0;
+	else
+		s->img->pixels[s->img->width * s->y + s->x] = 255;
+}
