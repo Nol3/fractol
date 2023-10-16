c857aeb300398752d7a881a81a3ca69bb2bbc260
Alejandro Cárdenas Parejo <alejandrocp_952@hotmail.com>
2023-10-16 14:09:09 +0200

    primer_update

---
+++ fractol/src/fractol.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alcarden <alcarden@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 13:30:46 by alcarden          #+#    #+#             */
+/*   Updated: 2023/10/16 13:41:13 by alcarden         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../MLX42/include/MLX42/MLX42.h"
+#include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory.h>
+
